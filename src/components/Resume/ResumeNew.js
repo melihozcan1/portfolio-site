bc83c5aaf8581a58b1d4aa63960d913eac3b8929
Melih Özcan <melihozcan98@hotmail.com>
2025-11-10 22:50:27 +0300

    link change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/melihozcan1/files/master/resume.pdf";
+  "https://raw.githubusercontent.com/melihozcan1/files/main/resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
